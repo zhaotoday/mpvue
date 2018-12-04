dd68c108b582304468ffd207dc721d337940b0f1
zhaojintian <729234283@qq.com>
2018-12-04 18:42:02 +0800

    update

---
+++ src/components/card/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-card">
+    card component
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style
+  lang="scss"
+  src="./styles/index.scss">
+</style>
---
+++ src/pages/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-index">
+    index
+    <Card />
+  </div>
+</template>
+
+<script>
+import Card from '@/components/card'
+
+export default {
+  components: {
+    Card
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles/index.scss">
+</style>

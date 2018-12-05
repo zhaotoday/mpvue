16a4abe684b3490094d3eab1b9fc4f12d38ce4a3
zhaojintian <729234283@qq.com>
2018-12-05 14:01:36 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from '@/app'
 import store from '@/store'
 import globalPlugin from '@/plugins/global'
+import '@/utils/init'
 
 Vue.config.productionTip = false
 
--- src/plugins/global.js
+++ src/plugins/global.js
@@ -1,9 +1,11 @@
 import consts from '@/utils/consts'
 import helpers from '@/utils/helpers/base'
+import wxb from '@/utils/wxb'
 
 export default {
   install (Vue) {
     Vue.prototype.$consts = consts
     Vue.prototype.$helpers = helpers
+    Vue.prototype.$wx = wxb
   }
 }

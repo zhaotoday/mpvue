9170c1867237ec0513e2034f9c6bdc3cf73b88ad
zhaojintian <729234283@qq.com>
2018-12-11 16:08:26 +0800

    优化

---
+++ src/components/swiper/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="c-swiper">
+    <swiper
+      class="c-swiper__swiper"
+      :circular="true"
+      :autoplay="true"
+      :interval="2500"
+      @change="handleChange">
+      <swiper-item
+        v-for="(item, index) in items"
+        :key="index">
+        <image
+          class="c-swiper__image"
+          mode="aspectFill"
+          :src="item.thumb" />
+        <div class="c-swiper__titles c1">
+          <div class="c-swiper__title f40">{{ item.title }}</div>
+          <div class="c-swiper__subtitle f30">{{ item.subtitle }}</div>
+        </div>
+      </swiper-item>
+    </swiper>
+    <div class="cc-indicator">
+      <span
+        v-for="(item, index) in indicatorItems"
+        :key="index"
+        :class="[ 'cc-indicator__item', { 'is-active': item === current } ]">
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      current: 0
+    }
+  },
+  computed: {
+    indicatorItems () {
+      return Array.from({ length: this.items.length }, (v, k) => k)
+    }
+  },
+  methods: {
+    handleChange (e) {
+      this.current = e.mp.detail.current
+    }
+  }
+}
+</script>
+
+<style
+  lang="scss"
+  src="./styles.scss">
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="p-index">
-    index
-    <Card />
+    <button
+      class="c-button c-button--2"
+      open-type="share">
+      推荐给好友
+    </button>
+    <CSwiper :items="[]" />
   </div>
 </template>
 
 <script>
-import Card from '@/components/card'
+import CSwiper from '@/components/swiper'
 
 export default {
   components: {
-    Card
+    CSwiper
   },
   async onShow () {
+    /*
     this.$wx.showToast({ title: '吐司' })
 
     await this.$helpers.sleep(2000)
@@ -21,6 +26,14 @@ export default {
       requiresLogin: true,
       url: '/pages/share/index'
     })
+    */
+  },
+  onShareAppMessage () {
+    return {
+      title: '还差一个帮帮砍就可以免费参加这个测试啦。麻烦你帮我打开点击帮砍一下，不胜感激，谢谢老板！',
+      path: '/pages/ask-for/index/main',
+      imageUrl: `${this.$consts.CDN_URL}/pages/ask-for/index/share.png`
+    }
   }
 }
 </script>

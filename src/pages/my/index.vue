90333eb7c47d69c734c0c3348be813689e406bbd
zhaojintian <729234283@qq.com>
2018-12-18 16:40:22 +0800

    优化

--- src/pages/my/index.vue
+++ src/pages/my/index.vue
@@ -6,14 +6,38 @@
         :src="user.avatarUrl" />
       <div class="pb-nickname c1 fs34">{{ user.nickName }}</div>
       <div class="pb-cell c-cell">
-        <div class="c-cell__item">
-          abc
+        <div class="c-cell__item fs28">
+          <div class="c-cell__icon c-icon c-icon--point"></div>
+          <div class="c-cell__label">积分</div>
+          <div class="c-cell__value">100</div>
         </div>
-        <div class="c-cell__item">
-          abc
+        <div class="c-cell__item fs28">
+          <div class="c-cell__icon c-icon c-icon--contact"></div>
+          <div class="c-cell__label">客服</div>
+          <div
+            class="c-cell__value"
+            data-text="13950442340"
+            @click="handleCopyTelephone">
+            13950442340(微信手机同号)
+          </div>
+          <!--
+          <div class="c-cell__addon-icon c-icon c-icon--call"></div>
+          -->
         </div>
-        <div class="c-cell__item">
-          abc
+        <div class="c-cell__item fs28">
+          <div class="c-cell__icon c-icon c-icon--phone"></div>
+          <div class="c-cell__label">手机号</div>
+          <div class="c-cell__value">13950442340</div>
+        </div>
+      </div>
+    </div>
+    <div class="pb-tabs c-tabs">
+      <div class="c-tabs__tabs fs32">
+        <div
+          v-for="item in pointTypes"
+          :key="item.value"
+          :class="[ 'c-tabs__tab', { 'is-active': item.value === '1' } ]">
+          {{ item.label }}
         </div>
       </div>
     </div>
@@ -25,6 +49,20 @@ export default {
   computed: {
     user () {
       return this.$auth.get().user
+    },
+    pointTypes () {
+      return this.$consts.POINT_TYPES
+    }
+  },
+  methods: {
+    async handleCopyTelephone (e) {
+      /*
+      await this.$wx.setClipboardData({
+        data: e.currentTarget.dataset.text
+      })
+
+      // this.$wx.showToast({ title: '已复制微信号，请返回微信添加好友' })
+      */
     }
   }
 }
--- src/utils/consts.js
+++ src/utils/consts.js
@@ -2,8 +2,20 @@ const API_URL = 'http://localhost:3002/apis'
 const API_VERSION = 'v1'
 const LOGIN_PAGE = '/pages/login/index'
 
+// 积分类型
+const POINT_TYPES = [
+  {
+    label: '分享积分',
+    value: '1'
+  },
+  {
+    label: '成交积分',
+    value: '2'
+  }
+]
 export default {
   API_URL,
   API_VERSION,
-  LOGIN_PAGE
+  LOGIN_PAGE,
+  POINT_TYPES
 }
